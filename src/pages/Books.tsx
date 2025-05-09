03ff6876a6cde764468c1fc84f75e4dbd11def47
shiba <2255070t@gsuite.kobe-u.ac.jp>
2025-05-09 17:51:04 +0900

    fix buildできるようにした

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,5 +1,4 @@
 import { useState, useMemo } from "react";
-import { Link } from "react-router-dom";
 import { useBookList } from "../hooks/useBooks";
 import { Book } from "../types/book";
 import { BookMemo } from "./BookMemo";
