ccafbbe4c40bea5f5963055e1d416b9c9ccda600
shiba <2255070t@gsuite.kobe-u.ac.jp>
2025-05-09 15:36:32 +0900

    add ローカルにあるmdファイルを表示する

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -2,28 +2,7 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
-const markdown = `
-# 見出し1
-
-これは **太字** のテキストです。
-
-- リスト項目 1
-    - sub
-- リスト項目 2
-
-1. first
-2. second
-
-
-[リンク](https://example.com)
-
-| 見出し1 | 見出し2 |
-|---------|---------|
-| 内容1   | 内容2   |
-
-- [x] 完了した項目
-- [ ] 未完了の項目
-`;
+import markdown from "../../docs/sample.md"
 
 export const MarkdownViewer: React.FC = () => {
   return (
