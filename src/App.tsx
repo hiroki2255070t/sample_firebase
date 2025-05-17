61230fc39477b41e0a6c6b5d202d4897379948b7
shiba <2255070t@gsuite.kobe-u.ac.jp>
2025-05-17 10:38:10 +0900

    fix filepath

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ProtectedRoute } from "./components/ProtectedRoute.tsx";
-import { Header } from "./components/Header.tsx";
-import { Root } from "./pages/Root.tsx";
-import { Home } from "./pages/Home.tsx";
-import { Login } from "./pages/Login.tsx";
-import { HistoryPage } from "./pages/History.tsx";
-import { HobbyPage } from "./pages/HobbyPage.tsx";
-import { JobPage } from "./pages/JobPage.tsx";
-import { JobDetail } from "./pages/JobDetail.tsx";
-import { Books } from "./pages/Books.tsx";
 import { SecretPage } from "./pages/SecretPage.tsx";
-import { HobbyDetail } from "./pages/HobbyDetail.tsx";
+import { Root } from "./pages/Root.tsx";
+import { Login } from "./pages/Login.tsx";
+import { Home } from "./pages/Home.tsx";
+import { Header } from "./components/Header.tsx";
+import { HistoryPage } from "./pages/History.tsx";
+import { JobPage } from "./pages/Job/JobPage.tsx";
+import { JobDetail } from "./pages/Job/JobDetail.tsx";
+import { Books } from "./pages/Book/Books.tsx";
+import { HobbyPage } from "./pages/Hobby/HobbyPage.tsx";
+import { HobbyDetail } from "./pages/Hobby/HobbyDetail.tsx";
 
 function App() {
   return (
