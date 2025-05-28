8ad696743d07bcade9b31f620be8b5187351ef8e
shiba <2255070t@gsuite.kobe-u.ac.jp>
2025-05-28 12:41:53 +0900

    fix 数式及びHTMLタグを表示できるようにする

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -1,10 +1,19 @@
-import ReactMarkdown from "react-markdown";
-import remarkGfm from "remark-gfm";
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import rehypeRaw from "rehype-raw";
+import 'katex/dist/katex.min.css';
 
 export const MarkdownViewer = ({ markdown }: { markdown: string }) => {
   return (
     <div className="prose p-4 py-8">
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm, remarkMath]}
+        rehypePlugins={[rehypeRaw, rehypeKatex]}
+      >
+        {markdown}
+      </ReactMarkdown>
     </div>
   );
 };
